ddcfc691d6a56eeaabef9f28905b93d41450833d
Dmitry Hramtsov <dhramcov562@gmail.com>
2025-04-20 17:23:09 +0500

    Прочие изменения

--- Source/poker_client/poker_client.Build.cs
+++ Source/poker_client/poker_client.Build.cs
@@ -8,7 +8,7 @@ public class poker_client : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
