b6d4db2c17c2a81cd671ecea8690bba07a3a64fe
Dmitry Hramtsov <dhramcov562@gmail.com>
2025-05-09 23:29:55 +0500

    Сделал дизайн основных виджетов

---
+++ Source/poker_client/Private/PokerPlayerController.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PokerPlayerController.h"
+#include "Blueprint/UserWidget.h" // Для CreateWidget и UUserWidget
+
+APokerPlayerController::APokerPlayerController()
+{
+    // Здесь можно установить значения по умолчанию, если нужно
+}
+
+void APokerPlayerController::BeginPlay()
+{
+    Super::BeginPlay(); // ВАЖНО: Вызвать родительский BeginPlay
+
+    // Ваша логика, которая была в Blueprint, может быть здесь или в BP наследнике
+    // Например, установка начального режима ввода для UI, если это всегда нужно при старте контроллера
+    // SetInputMode(FInputModeUIOnly()); // Или GameAndUI
+    // bShowMouseCursor = true;
+}
+
+void APokerPlayerController::ShowGameHUD()
+{
+    if (GameHUDClass)
+    {
+        if (GameHUDInstance && GameHUDInstance->IsInViewport()) return;
+        GameHUDInstance = CreateWidget<UUserWidget>(this, GameHUDClass);
+        if (GameHUDInstance)
+        {
+            GameHUDInstance->AddToViewport();
+            // Можно здесь установить режим ввода для игры с HUD
+            // FInputModeGameAndUI InputMode;
+            // InputMode.SetWidgetToFocus(GameHUDInstance->TakeWidget()); // Опционально для фокуса
+            // InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+            // SetInputMode(InputMode);
+            // bShowMouseCursor = true;
+        }
+    }
+}
+
+void APokerPlayerController::HideGameHUD()
+{
+    if (GameHUDInstance && GameHUDInstance->IsInViewport())
+    {
+        GameHUDInstance->RemoveFromViewport();
+        GameHUDInstance = nullptr;
+        // Можно здесь восстановить режим ввода только для игры
+        // SetInputMode(FInputModeGameOnly());
+        // bShowMouseCursor = false;
+    }
+}
---
+++ Source/poker_client/Public/PokerPlayerController.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PokerPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKER_CLIENT_API APokerPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+    APokerPlayerController(); // Конструктор
+
+    // Функции для управления HUD (как мы обсуждали для Дня 4)
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    void ShowGameHUD();
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    void HideGameHUD();
+
+protected:
+    virtual void BeginPlay() override; // Переопределяем BeginPlay
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+    TSubclassOf<class UUserWidget> GameHUDClass; // Класс для игрового HUD
+
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "UI")
+    class UUserWidget* GameHUDInstance;
+	
+};
