8573cc08761d4b2c0b89719a218f85776b27be9e
Dmitry Hramtsov <dhramcov562@gmail.com>
2025-05-18 18:33:31 +0500

    Создал и реализовал интерфейсы для карманных и настольных карт

---
+++ Source/poker_client/Private/HoleCardDisplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HoleCardDisplayInterface.h"
+
+// Add default functionality here for any IHoleCardDisplayInterface functions that are not pure virtual.
